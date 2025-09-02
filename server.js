54bb4275f0dfcc7dd1f5fa4cb744fb68c9484a46
Syed Faisal Amanulla <110450077+Syed-Faisal-Amanulla@users.noreply.github.com>
2025-09-03 01:27:24 +0530

    Update server.js

--- server.js
+++ server.js
@@ -19,6 +19,11 @@ mongoose.connect(process.env.MONGO_URI, {
 .then(() => console.log('MongoDB connected'))
 .catch((err) => console.log('MongoDB connection error:', err));
 
+// Root route
+app.get('/', (req, res) => {
+  res.send('Server is running!');
+});
+
 
 // Define Routes
 app.use('/api/auth', require('./routes/auth'));
@@ -29,5 +34,5 @@ app.use('/api/playlists', playlistRoutes);
 const PORT = process.env.PORT || 5000;
 
 app.listen(PORT, () => {
-  console.log(Server running on port ${PORT});
+  console.log(`Server running on port ${PORT}`);
 });
