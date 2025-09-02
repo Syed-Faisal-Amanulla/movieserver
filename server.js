b418f5a312ba22c6da76a018484c270a03a41dd0
Syed Faisal Amanulla <110450077+Syed-Faisal-Amanulla@users.noreply.github.com>
2025-09-03 01:36:14 +0530

    Update server.js

--- server.js
+++ server.js
@@ -19,14 +19,12 @@ mongoose.connect(process.env.MONGO_URI, {
 .then(() => console.log('MongoDB connected'))
 .catch((err) => console.log('MongoDB connection error:', err));
 
-/*
 
 // Root route
 app.get('/', (req, res) => {
   res.send('Server is running!');
 });
 
-*/
 
 // Define Routes
 app.use('/api/auth', require('./routes/auth'));
